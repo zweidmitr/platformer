d01ee93a13f0d4bb290ba8683ee7298b7fa8f077
zweidmitr <zweidmitr@gmail.com>
2022-06-27 13:14:58 +0300

    Episode #02

--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -11,6 +11,7 @@ public class GameWindow {
         jFrame.setSize(400, 400);
         jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
         jFrame.add(gamePanel);
+        jFrame.setLocationRelativeTo(null);
         jFrame.setVisible(true);
 
     }
